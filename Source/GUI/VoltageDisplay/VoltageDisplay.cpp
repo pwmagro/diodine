4e2182b93740c19b8b89f36f40ac187e80afc73e
Peter Magro <pwmagro@gmail.com>
2022-09-19 16:25:11 -0600

    clean up + comment VoltageDisplay.cpp

--- Source/GUI/VoltageDisplay/VoltageDisplay.cpp
+++ Source/GUI/VoltageDisplay/VoltageDisplay.cpp
@@ -14,120 +14,111 @@
 
 VoltageDisplay::VoltageDisplay(xynth::GuiData& g) : guiData(g)
 {
-    vfSlider.setSliderStyle(juce::Slider::SliderStyle::LinearHorizontal);
-    vfSlider.setTextBoxStyle(juce::Slider::NoTextBox, true, 0, 0);
-    vfSlider.setTextBoxIsEditable(true);
     auto& treeState = g.audioProcessor.treeState;
 
-    vfSlider.setName(VF_NAME);
-    vfSlider.setTextValueSuffix("V");
-    vfSlider.setNumDecimalPlacesToDisplay(1);
+    // Slider visuals
+    auto visualSetup = [](juce::Slider& slider, bool vertical, juce::String name) {
+        slider.setSliderStyle(vertical ? juce::Slider::SliderStyle::LinearVertical : juce::Slider::SliderStyle::LinearHorizontal);
+        slider.setTextBoxStyle(juce::Slider::NoTextBox, true, 0, 0);
+        slider.setTextBoxIsEditable(true);
+        slider.setName(name);
+        slider.setTextValueSuffix("V");
+        slider.setNumDecimalPlacesToDisplay(1);
+    };
+    visualSetup(vfSlider, false, VF_NAME);
+    visualSetup(vbSlider, false, VB_NAME);
+    visualSetup(satSlider, true, SAT_NAME);
 
+    // Slider attachments
     vfAttach = std::make_unique<juce::AudioProcessorValueTreeState::SliderAttachment>(treeState, VF_ID, vfSlider);
-
-    vfSlider.onValueChange = [this]() {
-        this->repaint();
-    };
-
-    addAndMakeVisible(vfSlider);
-
-
-    vbSlider.setSliderStyle(juce::Slider::SliderStyle::LinearHorizontal);
-    vbSlider.setTextBoxStyle(juce::Slider::NoTextBox, true, 0, 0);
-    vbSlider.setTextBoxIsEditable(true);
-
-    vbSlider.setName(VB_NAME);
-    vbSlider.setTextValueSuffix("V");
-    vbSlider.setNumDecimalPlacesToDisplay(1);
-
     vbAttach = std::make_unique<juce::AudioProcessorValueTreeState::SliderAttachment>(treeState, VB_ID, vbSlider);
-
-    vbSlider.onValueChange = [this]() {
-        this->repaint();
-    };
-
-    addAndMakeVisible(vbSlider);
-
-
-    satSlider.setSliderStyle(juce::Slider::SliderStyle::LinearVertical);
-    satSlider.setTextBoxStyle(juce::Slider::NoTextBox, true, 0, 0);
-    satSlider.setTextBoxIsEditable(true);
-
-    satSlider.setName(SAT_NAME);
-    satSlider.setTextValueSuffix("V");
-    satSlider.setNumDecimalPlacesToDisplay(1);
-
     satAttach = std::make_unique<juce::AudioProcessorValueTreeState::SliderAttachment>(treeState, SAT_ID, satSlider);
 
-    satSlider.onValueChange = [this]() {
-        this->repaint();
-    };
+    // Value change lambdas
+    auto onValueChange = [this]() { repaint(); };
+    vfSlider.onValueChange = onValueChange;
+    vbSlider.onValueChange = onValueChange;
+    satSlider.onValueChange = onValueChange;
 
+    // Register(?) sliders
+    addAndMakeVisible(vfSlider);
+    addAndMakeVisible(vbSlider);
     addAndMakeVisible(satSlider);
 
 };
 
 void VoltageDisplay::paint(juce::Graphics& g) {
+    
+    // rect = whole section box
     auto rect = getLocalBounds();
     auto& lnf = guiData.getLnf();
+
+    // draw background and add margins
     lnf.drawSectionBackground(g, rect);
     rect.reduce(10, 10);
 
+    // Saturation slider owns left 40px of the section
     auto satSliderRect = rect.removeFromLeft(40).withTrimmedBottom(80);
+    auto satTextRect = satSliderRect.translated(0, satSliderRect.getHeight() - 7.f);
+    g.drawText(((juce::String)SAT_NAME).toLowerCase(), satTextRect, juce::Justification::centredTop);
+    satSlider.setBounds(satSliderRect);
 
+    // Voltage slider labels own bottom 20px of the section
     auto labelRects = rect.removeFromBottom(20);
     auto vfLabelRect = labelRects.withTrimmedLeft(rect.getWidth() / 2).withTrimmedLeft(10);
     auto vbLabelRect = labelRects.withTrimmedRight(rect.getWidth() / 2).withTrimmedLeft(10);
 
+    // Draw slider labels
     g.setFont(lnf.getCustomFontRegular().withHeight(18));
     g.setColour(lnf.getAccent2());
     g.drawText(((juce::String)VF_NAME).toLowerCase(), vfLabelRect, juce::Justification::left);
     g.drawText(((juce::String)VB_NAME).toLowerCase(), vbLabelRect, juce::Justification::left);
-    // TODO draw sideways text for saturation bar name
 
+    // Voltage sliders own next bottom 40px of the section
     auto sliderRects = rect.removeFromBottom(40);
     auto vfSliderRect = sliderRects.withTrimmedLeft(rect.getWidth() / 2);
     auto vbSliderRect = sliderRects.withTrimmedRight(rect.getWidth() / 2);
-
-    auto satTextRect = satSliderRect.translated(0, satSliderRect.getHeight() - 7.f);
-    g.drawText(((juce::String)SAT_NAME).toLowerCase(), satTextRect, juce::Justification::centredTop);
-
     vfSlider.setBounds(vfSliderRect);
     vbSlider.setBounds(vbSliderRect);
-    satSlider.setBounds(satSliderRect);
 
+    // Slider values
     g.setFont(lnf.getCustomFontRegular().withHeight(25));
     g.setColour(lnf.getAccent1());
 
+    // Find the location of the slider knob
     auto vfSliderPos = vfSlider.getPositionOfValue(vfSlider.getValue());
     auto vbSliderPos = vbSlider.getPositionOfValue(vbSlider.getValue());
 
+    // Set a "wall" so labels don't go left or right of the slider
     auto newWidth = 80.f;
-    
     float vfx = vfSliderRect.getX();
     float vfy = vfSliderRect.getY() - 25.f;
     auto width = vfSlider.getWidth();
     juce::Rectangle<float> vfTextRect(std::min(std::max((vfSliderPos - 20.f), vfx + 10.f), vfx + width - 90.f), vfy, newWidth, 25.f);
-
     g.drawText(vfSlider.getTextFromValue(vfSlider.getValue()), vfTextRect, juce::Justification::centred);
 
     float vbx = vbSliderRect.getX();
     float vby = vbSliderRect.getY() - 25.f;
     width = vbSlider.getWidth();
     juce::Rectangle<float> vbTextRect(std::min(std::max((vbSliderPos - 20.f), vbx + 10.f), vbx + width - 90.f), vby, newWidth, 25.f);
-
     g.drawText(vbSlider.getTextFromValue(vbSlider.getValue()), vbTextRect, juce::Justification::centred);
 
-    drawWaveshaperLine(rect, g);
+    // Create the waveshaper graph
+    drawWaveshaper(rect, g);
     
 }
 
-void VoltageDisplay::drawWaveshaperLine(juce::Rectangle<int> rect, juce::Graphics& g) {
+void VoltageDisplay::drawWaveshaper(juce::Rectangle<int> rect, juce::Graphics& g) {
+    // Setup
     auto& lnf = guiData.getLnf();
     rect.reduce(10, 10);
     rect.removeFromBottom(20.f);
     auto gain = guiData.audioProcessor.treeState.getRawParameterValue(GAIN_ID)->load();
+    
+    // Create the background
     lnf.drawGraphBackground(g, rect.toFloat(), gain);
+    
+    // Get waveshaper output for each input from -1 to 1 and connect the dots
     waveshape.clear();
     auto& apvts = guiData.audioProcessor.treeState;
     auto firstPoint = juce::Point<float>(
@@ -141,9 +132,12 @@ void VoltageDisplay::drawWaveshaperLine(juce::Rectangle<int> rect, juce::Graphic
 
         waveshape.lineTo(point);
     }
+
+    // Draw the line
     g.setColour(WDYM::FgColor);
     g.strokePath(waveshape, juce::PathStrokeType(4.f, juce::PathStrokeType::curved));
 
+    // Draw a border
     g.setColour(WDYM::TextColor.darker(2));
     g.drawRoundedRectangle(rect.toFloat().expanded(2.f), 10.f, 4.f);
 }
